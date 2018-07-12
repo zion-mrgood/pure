3e6c0dee05b41968f5f3d6456587ecaf58fa9836
MrGood <sunjayking007@gmail.com>
2018-07-13 01:03:27 +0800

    修改webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 // 清理插件
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+// 合并
+const merge = require('webpack-merge');
 
 console.log('webpack.config.js')
 
@@ -19,7 +21,70 @@ const config = {
   webpack-merge
   CommonsChunkPlugin
   manifest
+  sideEffects tree-shaking 删除没有副作用的没有引用的代码
+  bundle-loader 用于分离代码和延迟加载
+  promise-loader 用promises 类似bundle-loader
+  
+  if (process.env.NODE_ENV !== 'production') {
+    doSomething...
+  }
+  
+  bundle分析
+  webpack-chart: webpack 数据交互饼图。
+  webpack-visualizer: 可视化并分析你的 bundle，检查哪些模块占用空间，哪些可能是重复使用的。
+  webpack-bundle-analyzer: 一款分析 bundle 内容的插件及 CLI 工具，以便捷的、交互式、可缩放的树状图形式展现给用户。
+  
+  懒加载 
+  
+  ProvidePugin
+  
+  
+  import 'babel-polyfill'
+  用whatwg-fetch，现代浏览器不加载babel-polyfill
+  polyfills.js
+  import 'babel-polyfill'
+  import 'whatwg-fetch'
+  
+  var modernBrowser = (
+    'fetch' in window &&
+    'assign' in Object
+  );
+  
+  if ( !modernBrowser ) {
+    var scriptElement = document.createElement('script');
+  
+    scriptElement.async = false;
+    scriptElement.src = '/polyfills.bundle.js';
+    document.head.appendChild(scriptElement);
+  }
+  
+  主版本号： 当API发生改变，并与之前的版本不兼容的时候
+  次版本号： 当增加了功能，但是向后兼容的时候
+  补丁版本号： 当做了向后兼容的缺陷修复的时候
+  
+  dev模式不要使用下面工具
+UglifyJsPlugin
+ExtractTextPlugin
+[hash]/[chunkhash]
+AggressiveSplittingPlugin
+AggressiveMergingPlugin
+ModuleConcatenationPlugin
+  
 */
+  // chunkFilename配置 异步加载
+  // return import(/* webpackChunkName */ 'lodash').then().catch()
+  // or
+  // async function func(){
+    // const _ = await import(/* webpackChunkName: "loadash" */ 'lodash');
+    // return el
+  // }
+  // func().then().catch();
+
+// ################# 以上是注释  
+
+// module.exports = merge(target, {
+  // devtool: 'inline-source-map'
+// })
 
 
 module.exports = {
@@ -44,11 +109,16 @@ module.exports = {
   mode: 'production',
   // 入口 entry
   entry: {
+    polyfills: 'polyfills.js',
     main: 'asd',
-    vendor: ''
+    vendor: [
+      'lodash'
+    ]
   },
-  // source-map
-  devtool: 'inline-source-map',
+  // 生产环境
+  devtool: 'source-map',
+  // 开发环境
+  devtool: 'none',
   // 开发服务器 dev-server
   devServer: {
     contentBase: './dist',
@@ -57,14 +127,15 @@ module.exports = {
   // 出口 output
   output: {
     // 输出文件名
-    filename: 'my-first-webpack.bundle.js',
-    filename: '[name][hash].js',
     filename: '[name][chunkhash].js',
     // 输出的绝对路径
     path: path.resolve('__dirname', 'dist'),
     path: path.join('__dirname', 'dist'),
     // 基础路径
-    publicPath: 'cdn'
+    publicPath: 'cdn',
+    // 非入口chunk
+    chunkFilename: '[name].bundle.js'
+    
   },
   // loader 编译
   module: {
@@ -110,7 +181,12 @@ module.exports = {
   // plugins 插件
   plugins: [
     // 压缩
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false
+      }
+    }),
     // 引用html模板
     new HtmlWebpackPlugin({
       title: 'test',
@@ -119,6 +195,7 @@ module.exports = {
     // chunk切块
     new webpack.optimize.CommonsChunkPlugin({
       name: ['vendor']
+      name: 'manifest'
     }),
     // 清理
     new CleanWebpackPlugin([
@@ -127,6 +204,23 @@ module.exports = {
     // HMR需要
     new webpack.NameModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
+    // 定义环境变量
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    // 分离Css
+    new ExtractTextPlugin({
+      filename: 'build.min.css',
+      allChunks: true
+    }),
+    // 保持未改动文件hash不变
+    new webpack.HasheModuleIdsPlugin(),
+    // 需要时引入，用时无需import
+    new webpack.ProvidePlugin({
+      // _: 'lodash'
+      // 可以直接用join lodash里的join，如_.join() 换成 join()
+      join: ['lodash', 'join']
+    })
   ]
   
   
